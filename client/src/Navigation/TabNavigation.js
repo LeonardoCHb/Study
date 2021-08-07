279909be1fc464ee215eec874551ab6eb6d32bc7
caio.maia@bitbucket.org <caio.maia@intelitrader.com.br>
2021-08-07 02:48:55 -0300

    Changing icons and color of tabs navigations

--- client/src/Navigation/TabNavigation.js
+++ client/src/Navigation/TabNavigation.js
@@ -2,14 +2,36 @@ import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from '../components/Home/HomeScreen';
 import { ClassScheduleScreen } from "../components/ClassSchedule/ClassScreduleScreen";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export function TabNavigation() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'Home') {
+                iconName = focused
+                    ? 'library-sharp'
+                    : 'library-outline';
+                } else if (route.name === 'Schedule') {
+                iconName = focused ? 'calendar' : 'calendar-outline';
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: '#D3D3D3',
+            tabBarInactiveTintColor: '#A9A9A9',
+            tabBarActiveBackgroundColor: '#000000',
+            tabBarInactiveBackgroundColor: '#000000',
+            })}
+        >
             <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Class Schedule" component={ClassScheduleScreen} />
+            <Tab.Screen name="Schedule" component={ClassScheduleScreen} />
         </Tab.Navigator>
     );
 }
