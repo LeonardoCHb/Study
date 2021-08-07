81f5245caa22a548436208404b64c0b5a0987245
caio.maia@bitbucket.org <caio.maia@intelitrader.com.br>
2021-08-07 18:22:03 -0300

    Adds a basic format to calendarScreen

--- client/src/components/Calendar/CalendarScreen.js
+++ client/src/components/Calendar/CalendarScreen.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styles  from "./CalendarScreen.style";
+import { Calendar } from "react-native-calendars";
 
 export function CalendarScreen() {
   return (
     <View style={styles.container}>
-      <Text>Your Class Calendar</Text>
-      <StatusBar style="auto" />
+        <Calendar 
+            theme={{
+                calendarBackground: styles.container.backgroundColor,
+                textDisabledColor: '#C0C0C0',
+                monthTextColor: '#00BFFF',
+                todayTextColor: '#00BFFF'
+            }}
+        />
+        <View style={{backgroundColor: 'blue'}}>
+            <Text>asdf</Text>
+        </View>
+        <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0078FF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
