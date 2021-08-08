c30e12c763366bb4e1a6558210f5d845507cb39c
caio.maia@bitbucket.org <caio.maia@intelitrader.com.br>
2021-08-07 22:56:31 -0300

    Fix calendar bug

--- client/src/components/Calendar/CalendarScreen.js
+++ client/src/components/Calendar/CalendarScreen.js
@@ -2,19 +2,10 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import styles  from "./CalendarScreen.style";
-import { Calendar } from "react-native-calendars";
 
 export function CalendarScreen() {
   return (
     <View style={styles.container}>
-        <Calendar 
-            theme={{
-                calendarBackground: styles.container.backgroundColor,
-                textDisabledColor: '#C0C0C0',
-                monthTextColor: '#00BFFF',
-                todayTextColor: '#00BFFF'
-            }}
-        />
         <View style={{backgroundColor: 'blue'}}>
             <Text>asdf</Text>
         </View>
