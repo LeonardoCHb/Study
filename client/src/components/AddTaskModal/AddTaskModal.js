eb934886238cdba8c29cc7dd19eabc59e21fccbd
leonardohably <leonardohably@hotmail.com>
2021-08-07 23:50:26 -0300

    Adds Task modal component styles

--- client/src/components/AddTaskModal/AddTaskModal.js
+++ client/src/components/AddTaskModal/AddTaskModal.js
@@ -1,10 +1,12 @@
 import * as React from "react";
-import { View } from "react-native";
-import { Button, Dialog, Portal, TextInput } from "react-native-paper";
+import { View, Text } from "react-native";
+import { Button, Dialog, Portal, TextInput, Checkbox } from "react-native-paper";
 
+import styles from "./AddTaskModal.styles";
 
 export function AddTaskModal({ visible, hideDialog }) {
-const [text, setText] = React.useState('');
+  const [text, setText] = React.useState("");
+  const [checked, setChecked] = React.useState(false);
 
   return (
     <View>
@@ -13,25 +15,45 @@ const [text, setText] = React.useState('');
           <Dialog.Title>Create your next activity!</Dialog.Title>
           <Dialog.Content>
             <TextInput
+              style={styles.input}
               placeholder="Activity Name"
               mode="outlined"
               value={text}
               onChangeText={(text) => setText(text)}
-              left={<TextInput.Icon name="new" />}
+              left={<TextInput.Icon name="text" />}
             />
             <TextInput
+              style={styles.input}
+              placeholder="Subject"
               mode="outlined"
               value={text}
               onChangeText={(text) => setText(text)}
+              left={<TextInput.Icon name="text" />}
             />
             <TextInput
+              style={styles.input}
+              placeholder="Description"
               mode="outlined"
               value={text}
               onChangeText={(text) => setText(text)}
+              left={<TextInput.Icon name="text" />}
             />
+            <View
+            style={styles.container}
+            >
+                <Checkbox
+                  status={checked ? "checked" : "unchecked"}
+                  onPress={() => {
+                    setChecked(!checked);
+                  }}
+                />
+                <Text
+                style={styles.text}
+                >It's a Exam</Text>
+            </View>
           </Dialog.Content>
           <Dialog.Actions>
-            <Button onPress={hideDialog}>Done</Button>
+            <Button onPress={hideDialog}>CREATE</Button>
           </Dialog.Actions>
         </Dialog>
       </Portal>
---
+++ client/src/components/AddTaskModal/AddTaskModal.styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    input: {
+      paddingBottom: 12,
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    text: {
+        alignSelf: 'center'
+    }
+    
+});
